762b3db3273fa6eca01addde45d8a20a16d94193
GiovanniPatierno <48275884+GiovanniPatierno@users.noreply.github.com>
2021-02-11 15:41:19 +0100

    Add files via upload
    final code

--- main.py
+++ main.py
@@ -1,31 +1,32 @@
-from sklearn.datasets import load_files
-from keras.applications.resnet50 import ResNet50, preprocess_input
-from IPython.core.display import Image, display
-from PIL import ImageFile
-from keras.applications.resnet50 import ResNet50
-from keras.utils import np_utils
-from keras.preprocessing import image
-from tqdm import tqdm
-from keras.applications.resnet50 import preprocess_input, decode_predictions
-from keras.layers import Conv2D, MaxPooling2D, GlobalAveragePooling2D
-from keras.layers import Dropout, Flatten, Dense
-from keras.models import Sequential
-from keras.callbacks import ModelCheckpoint
-import matplotlib.pyplot as plt
-from extract_bottleneck_features import *
-import os,cv2
+from glob import glob
 
 import numpy as np
-from glob import glob
+from keras.applications.resnet50 import preprocess_input
+from keras.callbacks import ModelCheckpoint
+from keras.layers import Dense
+from keras.layers import GlobalAveragePooling2D
+from keras.models import Sequential
+from keras.preprocessing import image
+from keras.utils import np_utils
+from sklearn.datasets import load_files
+from tqdm import tqdm
+
+from extract_bottleneck_features import *
+
+
+def Dog_Recognize_app(imgpath):
+    predict = Resnet50_prediction_breed(imgpath)
+    print("Questo cane sembra proprio appartenente alla razza: ")
+    print(dog_names[predict])
 
 
 def Resnet50_prediction_breed(img_path):
     # extract bottleneck features
     bottleneck_feature = extract_Resnet50(path_to_tensor(img_path))
     # obtain predicted vector
-    predicted_vector = ResNet50_model.predict(bottleneck_feature)
+    predicted_vector = Resnet50_model.predict(bottleneck_feature)
     # return dog breed that is predicted by the model
-    return dog_names[np.argmax(predicted_vector)]
+    return np.argmax(predicted_vector,-1)
 
 
 def get_bottleneck_features(path):
@@ -50,8 +51,6 @@ def paths_to_tensor(img_paths):
     return np.vstack(list_of_tensors)
 
 
-
-
 def test_model(model, test_tensors, test_targets, name):
     # get index of predicted dog breed for each image in test set
     predictions = [np.argmax(model.predict(np.expand_dims(tensor, axis=0))) for tensor in test_tensors]
@@ -72,7 +71,15 @@ def load_dataset(path):
 def ResNet50_predict_labels(img_path):
     # returns prediction vector for image located at img_path
     img = preprocess_input(path_to_tensor(img_path))
-    return np.argmax(ResNet50_model.predict(img))
+    return np.argmax(Resnet50_model.predict(img))
+
+
+# define function to load train, test, and validation datasets
+def load_dataset(path):
+    data = load_files(path)
+    dog_files = np.array(data['filenames'])
+    dog_targets = np_utils.to_categorical(np.array(data['target']), 133)
+    return dog_files, dog_targets
 
 
 # load train, test, and validation datasets
@@ -82,6 +89,7 @@ test_files, test_targets = load_dataset('dogImages/test')
 
 # load list of dog names
 dog_names = [item[20:-1] for item in sorted(glob("dogImages/train/*/"))]
+dog_names = np.array(dog_names)
 
 # print statistics about the dataset
 print('There are %d total dog categories.' % len(dog_names))
@@ -91,16 +99,6 @@ print('There are %d validation dog images.' % len(valid_files))
 print('There are %d test dog images.'% len(test_files))
 
 
-# define ResNet50 model
-ResNet50_model = ResNet50(weights='imagenet')
-
-ImageFile.LOAD_TRUNCATED_IMAGES = True
-
-# pre-process the data for Keras
-train_tensors = paths_to_tensor(train_files).astype('float32')/255
-valid_tensors = paths_to_tensor(valid_files).astype('float32')/255
-test_tensors = paths_to_tensor(test_files).astype('float32')/255
-
 train_Resnet50, valid_Resnet50, test_Resnet50 = get_bottleneck_features('bottleneck_features/DogResnet50Data.npz')
 
 Resnet50_model = Sequential()
@@ -108,19 +106,18 @@ Resnet50_model.add(GlobalAveragePooling2D(input_shape=(train_Resnet50.shape[1:])
 Resnet50_model.add(Dense(133, activation='softmax'))
 Resnet50_model.summary()
 
+
 Resnet50_model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
 
-Resnet50_model.fit(train_Resnet50, train_targets, validation_data=(valid_Resnet50, valid_targets), epochs=20, batch_size=20,  verbose=1)
+checkpointer = ModelCheckpoint(filepath='weights.best.Resnet50.hdf5', verbose=1, save_best_only=True)
+#Resnet50_model.fit(train_Resnet50, train_targets, validation_data=(valid_Resnet50, valid_targets),epochs=100, batch_size=20, callbacks=[checkpointer], verbose=1)
+Resnet50_model.load_weights('weights.best.Resnet50.hdf5')
+test_model(Resnet50_model,test_Resnet50, test_targets, 'Resnet50')
 
-Resnet50_model.save_weights("weightsProva.h5 ")
-
-Resnet50_model.load_weights("weightsProva.h5 ")
-
-test_model(Resnet50_model, test_Resnet50, test_targets, 'Resnet50')
-
-
-breed = Resnet50_prediction_breed('dogImages/train/118.Pembroke_welsh_corgi/Pembroke_welsh_corgi_07615.jpg')
-print(f"there is a dog and look like a {breed}\n")
-
-
-
+#Qui eseguo i test per riscontrate se effettivamente la macchina è accurata,controllando l output ricevuto dalla mia funziode di gestione Dog_Recognize_app
+Dog_Recognize_app('images/American_water_spaniel_00648.jpg')
+Dog_Recognize_app('images/Brittany_02625.jpg')
+Dog_Recognize_app('images/Curly-coated_retriever_03896.jpg')
+Dog_Recognize_app('dogImages/valid/004.Akita/Akita_00247.jpg')
+Dog_Recognize_app('dogImages/valid/039.Bull_terrier/Bull_terrier_02732.jpg')
+Dog_Recognize_app('dogImages/valid/054.Collie/Collie_03791.jpg')
